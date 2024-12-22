82a64a7112b7561ab831a86435fc477fda817db6
ji-sue <jayce9251@gmail.com>
2024-12-22 23:05:06 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+/** 
+ * Main template file
+ * 
+ * @package mytheme
+ * **/
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Theme</title>
+</head>
+<body>
+    
+</body>
+</html>
